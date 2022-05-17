eebf3da01e4f605781c8ebca43731ef66c4ad6d9
Olyazarichna <olya.zarichna@gmail.com>
2022-05-17 14:54:48 +0300

    розмітка

--- src/index.js
+++ src/index.js
@@ -1,3 +1,53 @@
 import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
+const URL = "https://restcountries.com/v2/name/peru";
+
+
+function fetchCountries(name) {
+   return fetch(URL).then(response => {
+       if(!response.ok) {
+           throw new Error(response.status);
+       }
+       console.log(response.json());
+       return response.json();
+   }).then(name => {
+       console.log(name); 
+   }).catch(error => error.message);
+};
+
+
+fetchCountries();
+
+
+// const searchParams = new URLSearchParams {
+//     // "name.official": name.official,
+//     "capital": capital,
+//     // "flag":flags.svg,
+
+//     languages,
+
+// }
+
+// name.official - повна назва країни
+// capital - столиця
+// population - населення
+// flags.svg - посилання на зображення прапора
+// languages - масив мов
+
+function markUpList() {
+    return` 
+    <li></li>`
+
+}
+
+function markUpDiv(){
+    return `
+    <img>
+    <p></p>
+    <p></p>
+    <p></p>
+    <p></p>
+    
+    `
+}
