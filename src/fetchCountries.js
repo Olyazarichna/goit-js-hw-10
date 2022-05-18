fed72738e7a8b3fddc4be641576fd0e4a9c241d8
Olyazarichna <olya.zarichna@gmail.com>
2022-05-18 16:40:07 +0300

    markup+

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,8 +1,10 @@
 export function fetchCountries(name) {
-    return fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`).then(response => {
-      if (!response.ok) {
-        throw new Error(response.status);
-      }
-      return response.json();
-    });
-  }
+  const URL = 'https://restcountries.com/v3.1/name/';
+  const fields = 'fields=name,capital,population,flags,languages';
+  return fetch(`URL${name}?${fields}`).then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  });
+}
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import './css/styles.css';
 import Notiflix from 'notiflix';
 
 import countryCard from './templates/country-card.hbs';
+import countryList from './templates/country-list.hbs';
 
 import { fetchCountries } from './fetchCountries.js';
 const DEBOUNCE_DELAY = 300;
@@ -11,7 +12,7 @@ const refs = {
   divEl: document.querySelector('.country-info'),
 };
  
- fetchCountries(name)
+ fetchCountries(2)
   .then(renderCountryCard)
   .catch(error => error.message);
 
