6128d6bee61a50e0307e0375821afabc9302da89
Olyazarichna <olya.zarichna@gmail.com>
2022-05-18 15:00:12 +0300

    mark up

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import './css/styles.css';
 import Notiflix from 'notiflix';
-import countryCard from './templates/country-card.hbs'
-
+// import countryCard from './templates/country-card.hbs'
+// import countryCard from '../templates/counrty-card.hbs'
+    
 const DEBOUNCE_DELAY = 300;
 
 const refs = {
@@ -21,6 +22,7 @@ const URL = "https://restcountries.com/v3.1/name/peru?fields=name,capital,popula
        console.log(name); 
        const markUp = countryCard(name);
        console.log(markUp);
+       refs.divEl.innerHTML = markUp;
    }).catch(error => error.message);
 
 
